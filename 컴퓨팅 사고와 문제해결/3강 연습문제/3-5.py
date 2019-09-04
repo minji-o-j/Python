e7fb4d8d5691046cc0f75103f52270fdc0bf6c40
Minji <jminji98@gmail.com>
2019-09-04 09:30:40 +0900

    Create 3-5.py

---
+++ 컴퓨팅 사고와 문제해결/3강 연습문제/3-5.py
@@ -0,0 +1,12 @@
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.setheading(45)
+t.forward(141)
+t.up()
+t.goto(0,0)
+t.down()
+t.setheading(0)
+t.forward(100)
+t.setheading(90)
+t.forward(100)
