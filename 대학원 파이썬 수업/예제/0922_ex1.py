1dcffb3a28fab2ada586682c06de31ab096fab8d
Minji Jung <jminji98@gmail.com>
2022-09-22 19:16:50 +0900

    0922 예제

---
+++ 대학원 파이썬 수업/예제/0922_ex1.py
@@ -0,0 +1,5 @@
+price =  50000
+print('원가: ',price)
+print( '판매가: ',price*0.85)
+
+print('적립금:',price*0.03)
---
+++ 대학원 파이썬 수업/예제/0922_ex3.py
@@ -0,0 +1,8 @@
+m = int(input("환전하고자 하는 금액을 입력하세요: "))
+usd = m/1408.50
+jpy = m/972.22
+gbp = m/1584.77
+
+print("미국: ",round(usd,2),"USD")
+print("일본: ",round(jpy,2),"JPY")
+print("영국: ",round(gbp,2),"GBP")
