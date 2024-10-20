5634410e77b095f8070bc47dc8b7d7686c1af15f
Minji Jung <jminji98@gmail.com>
2024-10-20 17:42:28 +0900

    W42 연속된 부분 수열의 합

---
+++ 연속된 부분 수열의 합.py
@@ -0,0 +1,21 @@
+# 투포인터
+def solution(sequence, k):
+    seqlen = len(sequence)
+    answer = []
+    
+    end = 0 # end pointer
+    checksum = 0
+    
+    for start in range(seqlen):
+        while checksum < k and end < seqlen: # end 늘리기
+            checksum += sequence[end]
+            end += 1
+        
+        if checksum == k:
+            answer.append([end-1-start,start, end-1])
+        
+        checksum -= sequence[start] # start 늘리기
+    
+    answer.sort()
+    
+    return answer[0][1:]
